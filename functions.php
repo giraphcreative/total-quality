19387982a17d434cfbcd4fef7aca2f1c87f5c92e
James Pederson <james@jpederson.com>
2015-03-03 19:58:23 -0600

    Initial commit.

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+
+
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 );
+
+
+// set a custom field prefix
+define( "CMB_PREFIX", "_p_" );
+
+
+// include some theme-related things
+include( "library/menus.php" );
+include( "library/scripts.php" );
+
+
+// an extra image manipulation function
+include( "library/images.php" );
+
+
+// include our metaboxes library
+include( "library/metabox.php" );
+
+
+// include quote metaboxes/functions
+include( "library/showcase.php" );
+include( "library/content.php" );
+
+
+?>
---
+++ js/src/main.js
@@ -0,0 +1,8 @@
+
+
+jQuery.ready(function(){
+	
+	
+	
+});
+
