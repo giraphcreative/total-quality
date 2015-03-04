3b72ec7800584a1f88e1330ea0f566f87cc81093
James Pederson <james@jpederson.com>
2015-03-03 22:35:54 -0600

    Adds header and footer partials to the templates.

--- footer.php
+++ footer.php
@@ -9,31 +9,34 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-	<footer id="colophon" class="footer" role="contentinfo">
-		<div class="site-info">
+		</main>
+
+		<footer class="bg-blue-dark group">
 			
-			<div class="social-icons">
-				<a href="http://www.facebook.com/ripon.college"><img src="<?php print get_template_directory_uri() ?>/img/social-facebook.png"></a>
-				<a href="https://twitter.com/riponcollege"><img src="<?php print get_template_directory_uri() ?>/img/social-twitter.png"></a>
-				<a href="http://www.youtube.com/riponcollegevideo"><img src="<?php print get_template_directory_uri() ?>/img/social-youtube.png"></a>
-				<a href="http://instagram.com/riponcollege"><img src="<?php print get_template_directory_uri() ?>/img/social-instagram.png"></a>
-				<a href="http://www.pinterest.com/riponcollege/"><img src="<?php print get_template_directory_uri() ?>/img/social-pinterest.png"></a>
-				<a href="http://riponcollegeadmission.tumblr.com/"><img src="<?php print get_template_directory_uri() ?>/img/social-tumblr.png"></a>
-				<a href="https://www.flickr.com/photos/ripon_college/"><img src="<?php print get_template_directory_uri() ?>/img/social-flickr.png"></a>
-				<a href="http://www.linkedin.com/groups?home=&gid=4646327&trk=anet_ug_hm"><img src="<?php print get_template_directory_uri() ?>/img/social-linkedin.png"></a>
+			<div class="third location">
+				<p>931 E. Walnut St., Suite 204 (map)<br>
+					Pasadena, CA 91106<br>
+					Monday-Friday 8:00 a.m.-5:00 p.m. PT<br>
+					Phone: 626.765.6980</p>
 			</div>
-			
-			<nav role="navigation">
-				<?php wp_nav_menu( array( 
-					'theme_location' => 'footer', 
-					'menu_class' => 'nav-menu' ) 
-				); ?>
-			</nav>
+			<div class="third contact">
+				<p>Fax to Email: 626.765.6975<br>
+					General Inquiries<br>
+					Email: slb@totalqualityconsulting.com<br>
+					Financial: DQD@TotalQualityConsulting.com</p>
+			</div>
+			<div class="third social">
+				<a href="#"><img src="<?php print get_template_directory_uri() ?>/img/social-facebook.png"></a>
+				<a href="#"><img src="<?php print get_template_directory_uri() ?>/img/social-linkedin.png"></a>
+			</div>
 
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		</footer>
 
-</div><!-- #page -->
+		<div class="copyright">
+			Total Quality Consulting &copy; 2015 
+		</div>
+
+	</div>
 
 <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -6,14 +6,10 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-<title><?php wp_title( '|', true, 'right' ); ?> Ripon College Viewbook</title>
+<title><?php wp_title( '|', true, 'right' ); ?> Total Quality Consulting</title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
 
 <?php wp_head(); ?>
 <link href="<?php bloginfo( "template_url" ) ?>/css/main.css" rel="stylesheet" type="text/css">
@@ -23,16 +19,19 @@
 
 <div class="wrap">
 
-	<header>
+	<div class="container wrap group">
+		<header>
 
-		<div class="logo">
-			<a href="<?php bloginfo( "home" ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<img src="<?php bloginfo( "template_url" ) ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>">
-			</a>
-		</div>
+			<div class="logo">
+				<a href="<?php bloginfo( "home" ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<img src="<?php bloginfo( "template_url" ) ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>">
+				</a>
+			</div>
 
-		<nav role="navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-		</nav>
+			<nav role="navigation">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+			</nav>
 
-	</header>
+		</header>
+
+		<main>
