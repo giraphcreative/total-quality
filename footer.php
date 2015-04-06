84934d55bd440db5d4b67033387595660eadc694
James Pederson <james@jpederson.com>
2015-04-05 21:52:07 -0500

    Update footer link styles, email address formatting, and added fax number.

--- footer.php
+++ footer.php
@@ -17,13 +17,14 @@
 				<p>931 E. Walnut St., Suite 204 (map)<br>
 					Pasadena, CA 91106<br>
 					Monday-Friday 8:00 a.m.-5:00 p.m. PT<br>
-					Phone: 626.765.6980</p>
+					Phone: <a href="tel:6267656980">626.765.6980</a></p>
 			</div>
 			<div class="third contact">
 				<p>Fax to Email: 626.765.6975<br>
+					Dedicated Fax: 626.356.0001<br>
 					General Inquiries<br>
-					Email: slb@totalqualityconsulting.com<br>
-					Financial: DQD@TotalQualityConsulting.com</p>
+					Email: <a href="mailto:slb@TotalQualityConsulting.com">slb@TotalQualityConsulting.com</a><br>
+					Financial: <a href="mailto:dqd@TotalQualityConsulting.com">dqd@TotalQualityConsulting.com</a></p>
 			</div>
 			<div class="third social">
 				<a href="https://www.google.com/url?q=https%3A%2F%2Fwww.facebook.com%2FTotalQualityConsultingInc&sa=D&sntz=1&usg=AFQjCNFhsTWjGzeLStTIM_vmDFwJAiqksg" target="_blank"><img src="<?php print get_template_directory_uri() ?>/img/social-facebook.png"></a>
