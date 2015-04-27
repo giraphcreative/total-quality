de9a45d4e7732daaed64c85586fd065299531954
James Pederson <james@jpederson.com>
2015-04-27 09:26:09 -0500

    Add login logo styles and white box around single page content.

--- single.php
+++ single.php
@@ -8,7 +8,8 @@ get_header();
 ?>
 	<div id="primary" class="site-content">
 
-		<div id="content" class="site-content content-narrow" role="main">
+		<div class="white-box group">
+			<div class="content-narrow">
 		<?php 
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post(); 
@@ -18,7 +19,8 @@ get_header();
 				the_content();
 			endwhile;
 		endif;
-		 ?>
+		?>
+			</div>
 		</div><!-- #content -->
 
 	</div><!-- #primary -->
